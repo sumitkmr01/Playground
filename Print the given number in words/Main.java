c22028e4ea3fc9768892c23d1f32b89ce9beda34
sumitkmr01 <30625196+sumitkmr01@users.noreply.github.com>
2019-01-20 12:23:32 +0530

    Test code pushed to repository.

---
+++ Print the given number in words/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class Main {
+	public static void main (String[] args) {
+      
+	    // Type your code here
+      Scanner in=new Scanner(System.in);
+      int a=in.nextInt();
+      if(a==1)
+        System.out.println("one");
+      else if(a==2)
+        System.out.println("two");
+      else if(a==3)
+        System.out.println("Three");
+      else if(a==4)
+        System.out.println("four");
+      else if(a==5)
+        System.out.println("five");
+      
+      else
+        System.out.println("Invalid");
+      
+	}
+}
